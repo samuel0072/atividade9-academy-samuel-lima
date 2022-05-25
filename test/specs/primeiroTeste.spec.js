ed909ec853c2c96db0e4b1e30f405a58ec1ac840
Samuel Lima <samuelima.00@outlook.com>
2022-05-25 18:21:13 -0300

    Adiciona configuração do WDIO e um primeiro teste

---
+++ test/specs/primeiroTeste.spec.js
@@ -0,0 +1,5 @@
+describe("Primeiro teste", () => {
+    it("deve vefificar o primeiro teste", () => {
+        
+    });
+});
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 exports.config = {
     //
     // ====================
@@ -51,18 +53,11 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-    
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
+        platformName: "Android",
+        app: path.join(__dirname, "app", "apidemos.apk"),
+        automationName: "UiAutomator2",
+        appPackage: "io.appium.android.apis", //nome do pacote do App
+        path: "/wd/hub"
     }],
     //
     // ===================
